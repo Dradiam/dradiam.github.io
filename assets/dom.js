27e62eeb6ea02734c966849b95a3daa4877c2f09
A. M. <71303276+Dradiam@users.noreply.github.com>
2026-06-24 15:11:26 +0800

    Delete assets directory

--- assets/dom.js
+++
@@ -1,7 +0,0 @@
-
-export const dom = {
-    statusMessage: document.querySelector(".status-message"),
-    terminalMenu: document.querySelector(".terminal-menu"),
-    contentViewer: document.querySelector(".content-viewer"),
-    widgetContainer: document.querySelector(".widget-container")
-};
--- assets/main.js
+++
@@ -1,6 +0,0 @@
-
-import { config } from './config.js';
-import { init, setupEventListeners } from './events.js';
-
-setupEventListeners();
-init(config.DEFAULT_ACCOUNT);

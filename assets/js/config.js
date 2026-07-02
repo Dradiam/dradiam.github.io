4e0de78e69252afcf33da23c16743cf84479145f
A. M. <71303276+Dradiam@users.noreply.github.com>
2026-07-02 15:54:01 +0800

    Update DEFAULT_ACCOUNT path in config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
 
 export const config = {
     VERSION_NUMBER: 1.3,
-    DEFAULT_ACCOUNT: 'accounts/default.json',
+    DEFAULT_ACCOUNT: 'accounts/main.json',
     DIRECTORY_PATH: './accounts/',
     DEFAULT_FILE: 'main',
     VIEWER_TYPES: ['txt', 'html', 'img'],
